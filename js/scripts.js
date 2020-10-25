6944f1d8a9f6b0a0be4637414d1a9b1875bf121a
V-Donut <vikaevt123@gmail.com>
2020-10-25 13:12:05 +0300

    Модальное окно добавления в корзину

--- js/scripts.js
+++ js/scripts.js
@@ -1,96 +1,138 @@
 const mapLink = document.querySelector("#map-link");
 const mapPopup = document.querySelector(".map-popup");
-const mapClose = mapPopup.querySelector(".modal-close");
 
 const buttonContacts = document.querySelector(".button-contacts");
 const feedbackModal = document.querySelector(".write-us");
-const feedbackClose = feedbackModal.querySelector(".modal-close");
-const feedbackForm = feedbackModal.querySelector(".write-us-form");
-const feedbackName = feedbackModal.querySelector("#your-name");
-const feedbackEmail = feedbackModal.querySelector("#your-email");
-const feedbackMessage = feedbackModal.querySelector("#your-message");
 
-let isStorageSupport = true;
-let name, email;
+const cartButtons = document.querySelectorAll(".button-cart");
+const cartModal = document.querySelector(".cart-modal");
 
-// Чтение данных из localStorage
-try {
-  name = localStorage.getItem("feedbackName");
-  email = localStorage.getItem("feedbackEmail");
-} catch (err) {
-  isStorageSupport = false;
+if (feedbackModal) {
+  const feedbackClose = feedbackModal.querySelector(".modal-close");
+  const feedbackForm = feedbackModal.querySelector(".write-us-form");
+  const feedbackName = feedbackModal.querySelector("#your-name");
+  const feedbackEmail = feedbackModal.querySelector("#your-email");
+  const feedbackMessage = feedbackModal.querySelector("#your-message");
+
+  let isStorageSupport = true;
+  let name, email;
+
+  // Чтение данных из localStorage
+  try {
+    name = localStorage.getItem("feedbackName");
+    email = localStorage.getItem("feedbackEmail");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  // Открытие окна с формой обратной связи
+  buttonContacts.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    feedbackModal.classList.add("modal-show");
+  
+    if (isStorageSupport && name && email) {
+        // Подстановка данных из localStorage
+        feedbackName.value = name;
+        feedbackEmail.value = email;
+        feedbackMessage.focus();
+    } else {
+      feedbackName.focus();
+    }
+  });
+
+  // Закрытие окна с формой обратной связи
+  feedbackClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    feedbackModal.classList.remove("modal-show");
+    feedbackModal.classList.remove("modal-error");
+  });
+
+  // Отправка формы обратной связи
+  feedbackForm.addEventListener("submit", function (evt) {
+    if (!feedbackName.value || !feedbackEmail.value || !feedbackMessage.value) {
+      evt.preventDefault();
+
+      // Анимация формы, если хотя бы одно поле не заполнено
+      feedbackModal.classList.remove("modal-error");
+      feedbackModal.offsetWidth = feedbackModal.offsetWidth;
+      feedbackModal.classList.add("modal-error");
+
+      // Фокусировка на незаполненном поле
+      if (!feedbackName.value) {
+        feedbackName.focus();
+      } else if (!feedbackEmail.value) {
+        feedbackEmail.focus();
+      } else {
+        feedbackMessage.focus();
+      }
+    } else {
+      // Сохранение в localStorage
+      if (isStorageSupport) {
+        localStorage.setItem("feedbackName", feedbackName.value);
+        localStorage.setItem("feedbackEmail", feedbackEmail.value);
+      }
+    }
+  });
 }
 
-// Открытие окна с формой обратной связи
-buttonContacts.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  feedbackModal.classList.add("modal-show");
+if (mapPopup) {
+  const mapClose = mapPopup.querySelector(".modal-close");
 
-  if (isStorageSupport && name && email) {
-      // Подстановка данных из localStorage
-      feedbackName.value = name;
-      feedbackEmail.value = email;
-      feedbackMessage.focus();
-  } else {
-    feedbackName.focus();
+  mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+  });
+  
+  mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+  });
+}
+
+if (cartModal) {
+  const cartClose = cartModal.querySelector(".modal-close");
+  const cartLink = document.querySelector(".cart-link");
+
+  for (let i = 0; i < cartButtons.length; i++) {
+    cartButton = cartButtons[i];
+
+    cartButton.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      cartModal.classList.add("modal-show");
+
+      if (!cartLink.classList.contains("full-cart")) {
+        cartLink.classList.add("full-cart");
+      }
+
+      const cartCounter = document.querySelector("#cart-counter");
+      if (cartCounter) {
+        cartCounter.innerHTML = Number(cartCounter.innerHTML) + 1;
+      }
+    });
   }
-});
 
-// Закрытие окна с формой обратной связи
-feedbackClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  feedbackModal.classList.remove("modal-show");
-  feedbackModal.classList.remove("modal-error");
-});
+  cartClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartModal.classList.remove("modal-show");
+  });
+}
 
-// Открытие окна с формой обратной связи клавишей Esc
+// Закрытие модальных окон клавишей Esc
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-    if (feedbackModal.classList.contains("modal-show")) {
+    if (feedbackModal && feedbackModal.classList.contains("modal-show")) {
       evt.preventDefault();
       feedbackModal.classList.remove("modal-show");
     }
 
-    if (mapPopup.classList.contains("modal-show")) {
+    if (mapPopup && mapPopup.classList.contains("modal-show")) {
       evt.preventDefault();
       mapPopup.classList.remove("modal-show");
     }
-  }
-});
 
-// Отправка формы обратной связи
-feedbackForm.addEventListener("submit", function (evt) {
-  if (!feedbackName.value || !feedbackEmail.value || !feedbackMessage.value) {
-    evt.preventDefault();
-
-    // Анимация формы, если хотя бы одно поле не заполнено
-    feedbackModal.classList.remove("modal-error");
-    feedbackModal.offsetWidth = feedbackModal.offsetWidth;
-    feedbackModal.classList.add("modal-error");
-
-    // Фокусировка на незаполненном поле
-    if (!feedbackName.value) {
-      feedbackName.focus();
-    } else if (!feedbackEmail.value) {
-      feedbackEmail.focus();
-    } else {
-      feedbackMessage.focus();
-    }
-  } else {
-    // Сохранение в localStorage
-    if (isStorageSupport) {
-      localStorage.setItem("feedbackName", feedbackName.value);
-      localStorage.setItem("feedbackEmail", feedbackEmail.value);
+    if (cartModal && cartModal.classList.contains("modal-show")) {
+      evt.preventDefault();
+      cartModal.classList.remove("modal-show");
     }
   }
-});
-
-mapLink.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  mapPopup.classList.add("modal-show");
-});
-
-mapClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  mapPopup.classList.remove("modal-show");
 });
